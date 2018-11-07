25ce8cc83a6d12110aae595875a0761402ed6119
alrenucsd <alren@ucsd.edu>
2018-11-06 20:33:34 -0800

    Added carousel navigation

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react';
 import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'; // animation wrapper
-import CarouselImage from './CarouselImage.js';
+import CarouselImage from './CarouselImage';
 // Gotta find an easier way to import multiple images
 import tmpbanner from '../img/tmpbanner.png';
 import tmpbanner2 from '../img/tmpbanner2.png';
@@ -36,12 +36,39 @@ const Animations = styled.div`
 
 `;
 
+const CarouselNavStyle = styled.div`
+    position: absolute;
+    display: block;
+    left: 50%;
+    transform: translate(-50%);
+    bottom: 10px;
+    margin: 0 auto;
+`;
+
+const Dot = styled.span`
+    cursor: pointer;    
+    height: 12px;
+    width: 12px;
+    border-radius: 50%;
+    margin: 10px 6px;
+    background-color: #FFF;
+    display: inline-block;
+    transition: background-color 0.6s ease;
+
+    &:hover {
+        background-color: #686585;
+    }
+`;
+
+var interval;
+
 class Carousel extends Component {
 	constructor(props){
         super(props);
         
         this.state = {
-            currentImageIndex: 0
+            currentImageIndex: 0,
+            sliding: false
         };
 
         this.setSlide = this.setSlide.bind(this);
@@ -60,14 +87,22 @@ class Carousel extends Component {
         }
 
         this.setState({
-            currentImageIndex: index
+            currentImageIndex: index,
+            sliding: true
         });
+
+        // timer which prevents changing while sliding
+        setTimeout(() => {
+            this.setState({
+               sliding: false
+             })
+           }, 500)
 
     }
 
     /* As soon as it mounts, start a timer which changes slides periodically */
     componentDidMount() {
-        setInterval(
+        interval = setInterval(
             function() {
                 var { currentImageIndex } = this.state;
                 this.setSlide(currentImageIndex + 1)
@@ -77,6 +112,24 @@ class Carousel extends Component {
     }
     
 	render(){
+        const dots = imgUrls.map((url, index) => {
+            return <Dot key={url} onClick={() => {
+                // only sets slide if it's not currently sliding
+                if(!this.state.sliding) {
+                    this.setSlide(index);
+                    // start over auto timer
+                    clearInterval(interval);
+                    interval = setInterval(
+                        function() {
+                            var { currentImageIndex } = this.state;
+                            this.setSlide(currentImageIndex + 1)
+                        }
+                        .bind(this),
+                        4000);
+                }
+            }} />;
+        });
+
 		return (
 			<CarouselContainer>
                 <Animations>
@@ -93,6 +146,9 @@ class Carousel extends Component {
                 </Animations>
 
                 {/* TODO: complete carousel navigation */}
+                <CarouselNavStyle>
+                    {dots}
+                </CarouselNavStyle>
             </CarouselContainer>
 		);
 	}
