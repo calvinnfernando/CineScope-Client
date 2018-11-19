a0ea433c010451f34ef81ed1f83911b38f31fe00
Bethany Bin <bethanybin@gmail.com>
2018-11-18 17:07:38 -0800

    embedded video

--- src/containers/MoviePage/index.js
+++ src/containers/MoviePage/index.js
@@ -29,15 +29,23 @@ class MoviePage extends Component {
         <div style={{display: "inline-block", verticalAlign: "top"}}>
           <div style={{display: "flex", alignItems: "center", flexDirection:"column"}}>
             <h1>Iron Man 3</h1>
+            <h2>2013</h2>
             <img src={TestImage} alt='test' style={{width: 300}}/>
             <div style={{justifyContent: "center"}}>
-              <button onClick={this.handleThumbsUp} style={{border: "none", cursor: "pointer"}}><img src={ThumbsUp} alt=''/></button>
-              <button onClick={this.handleThumbsDown} style={{border: "none", cursor: "pointer"}}><img src={ThumbsDown} alt=''/></button>
+              <button onClick={this.handleThumbsUp} style={{border: "none", cursor: "pointer", backgroundColor: "Transparent"}}><img src={ThumbsUp} alt=''/></button>
+              <button onClick={this.handleThumbsDown} style={{border: "none", cursor: "pointer", backgroundColor: "Transparent"}}><img src={ThumbsDown} alt=''/></button>
             </div>
+            <h4>Average rating: 7/10</h4>
           </div>
         </div>
         <div style={{display: "inline-block", paddingLeft: 50}}>
+          <iframe height="480" width="640"
+            src="https://www.youtube.com/embed/oYSD2VQagc4">
+          </iframe>
           <p>This is a description of the movie</p>
+        </div>
+        <div style = {{display: "inline-block", paddingRight: 100}}>
+          <button style={{cursor: "pointer", backgroundColor: "Transparent"}}>Compare</button>
         </div>
       </div>
     )
