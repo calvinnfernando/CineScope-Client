7c88da75667d8510e4d74f5a5167b36b6f2c73f9
alizawren <alissaren98@gmail.com>
2018-11-24 22:45:52 -0800

    Used MovieCard from moviecomponent-dev branch (which properly routes)

--- src/components/MovieLists/MovieCard.js
+++ src/components/MovieLists/MovieCard.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Route } from 'react-router-dom';
-import MoviePage from '../../containers/MoviePage';
 import '../../styles/components/movieCard.css';
 
 const MovieCard = (props) => (
@@ -10,8 +8,8 @@ const MovieCard = (props) => (
           <img className="card-img-top movie-img"
             src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${props.movie.poster_path}`}
             onError={(e) => {e.target.src="https://i.imgur.com/SeLMJwk.png"}} alt="" width="200" height="300" />
-            <Route path={"/" + props.movie.title.split(" ").join("_")} component={MoviePage}/>
-            <a href={"/" + props.movie.title.split(" ").join("_")}>
+           
+            <a href={"/movie/" + parseInt(props.movie.id)}>
               <div className="card-img-overlay movie-description">
                   <p className="card-text">{props.movie.title}</p>
               </div>
