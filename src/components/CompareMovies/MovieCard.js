b085367fcf86d247b83b8e44c2e99875689735a3
alizawren <alissaren98@gmail.com>
2018-12-01 19:54:51 -0800

    Reorganized using styled components, unfinished

--- src/components/CompareMovies/MovieCard.js
+++ src/components/CompareMovies/MovieCard.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import '../../styles/components/movieCard.css';
 import styled from 'styled-components';
@@ -9,24 +9,39 @@ const SelectedMovieStyle = styled.div`
   border-color: white;
 `;
 
+const MovieCardStyle = styled.div`
+width: 80%;
+
+`;
+
+const MovieCardOverlay = styled.div`
+transition: .5s ease;
+opacity: 0;
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+-ms-transform: translate(-50%, -50%);
+text-align: center;
+`;
+
 class MovieCard extends Component {
   constructor(props) {
-      super(props);
+    super(props);
 
-      this.state = {
-        selected: false
-      };
-      this.toggleSelect = this.toggleSelect.bind(this);
+    this.state = {
+      selected: false
+    };
+    this.toggleSelect = this.toggleSelect.bind(this);
 
   }
 
   toggleSelect() {
-    if (this.state.selected === true)
-    {
-      this.setState({selected: false});
+    if (this.state.selected === true) {
+      this.setState({ selected: false });
       console.log("set to false");
     } else {
-      this.setState({selected: true});
+      this.setState({ selected: true });
       console.log("set to true");
     }
   }
@@ -35,40 +50,30 @@ class MovieCard extends Component {
     if (this.state.selected) {
       return (
 
-      <div className="movie-card" onClick={() => {this.props.selectMovie(this.props.movie); this.toggleSelect();}}>
+        <MovieCardStyle onClick={() => { this.props.selectMovie(this.props.movie); this.toggleSelect(); }}>
+          <SelectedMovieStyle>
+            <img className="card-img-top movie-img"
+              src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${this.props.movie.poster_path}`}
+              onError={(e) => { e.target.src = "https://i.imgur.com/SeLMJwk.png" }} alt="" width="200" height="300" />
 
-          <div className="movie-card card">
-<SelectedMovieStyle>
-              <img className="card-img-top movie-img"
-                src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${this.props.movie.poster_path}`}
-                onError={(e) => {e.target.src="https://i.imgur.com/SeLMJwk.png"}} alt="" width="200" height="300" />
-
-
-                  <div className="card-img-overlay movie-description">
-                      <p className="card-text">{this.props.movie.title}</p>
-                  </div>
-  </SelectedMovieStyle>
-          </div>
-
-      </div>
+            <MovieCardOverlay>
+              <p className="card-text">{this.props.movie.title}</p>
+            </MovieCardOverlay>
+          </SelectedMovieStyle>
+        </MovieCardStyle>
 
       );
     }
     return (
-    <div className="movie-card" onClick={() => {this.props.selectMovie(this.props.movie); this.toggleSelect();}}>
-        <div className="movie-card card">
+      <MovieCardStyle onClick={() => { this.props.selectMovie(this.props.movie); this.toggleSelect(); }}>
+          <img className="card-img-top movie-img"
+            src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${this.props.movie.poster_path}`}
+            onError={(e) => { e.target.src = "https://i.imgur.com/SeLMJwk.png" }} alt="" width="200" height="300" />
+          <MovieCardOverlay>
+            <p className="card-text">{this.props.movie.title}</p>
+          </MovieCardOverlay>
 
-            <img className="card-img-top movie-img"
-              src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${this.props.movie.poster_path}`}
-              onError={(e) => {e.target.src="https://i.imgur.com/SeLMJwk.png"}} alt="" width="200" height="300" />
-
-
-                <div className="card-img-overlay movie-description">
-                    <p className="card-text">{this.props.movie.title}</p>
-                </div>
-
-        </div>
-    </div>
+      </MovieCardStyle>
     );
   }
 }
