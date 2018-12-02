afa7b6e89d53aa59a32aaa44d84ce193f211f029
KevenSaputra <ksaputra@ucsd.edu>
2018-12-02 13:35:35 -0800

    Final implementation of dropdown box with reactstrap

--- src/components/Header/MenuButton.js
+++ src/components/Header/MenuButton.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import SignOutButton from '../SignOut';
-
 import profilepic from '../img/profile.svg';
+
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 const MenuBtnStyle = styled.div`
     position: relative;
@@ -56,75 +57,47 @@ const IconContainerStyle = styled.div`
       }
 `;
 
-const WrapperDropdown = styled.div`
-    /* Size and position */
-    position: relative; 
-    width: 200px;
-    padding: 10px;
-    margin: 0 auto;
-
-    /* Styles */
-    color: #fff;
-    outline: none;
-`;
-
-const Dropdown = styled.ul`
-    /* Size & position */
-    position: relative;
-
-    /* Styles */
-    background: #fff;
-    font-weight: normal; /* Overwrites previous font-weight: bold; */
-
-    /* Hiding */
-    opacity: 0;
-    pointer-events: none;
-
-    &:active {
-        opacity: 1;
-        pointer-events: auto;
-    }
-`;
-
-const ItemMenu = styled.li`
-    display: block;
-    text-decoration: none;
-    color: #9e9e9e;
-    padding: 10px 20px;
-
-    &:hover {
-        background: #f3f8f8;
-    }
-`;
-
 class MenuButton extends Component {
     constructor() {
         super();
-        this.handleClick = this.handleClick.bind(this);
+
+        this.state = {
+            dropdownOpen: false
+        };
+
+        this.toggle = this.toggle.bind(this);
     }
 
-    handleClick(event) {
-        alert("tes");
+    toggle() {
+        this.setState({
+            dropdownOpen: !this.state.dropdownOpen
+        });
     }
 
     render() {
         return (
             <MenuBtnStyle>
-                <WrapperDropdown >
-                    {   /* renders text or account icon */
-                        this.props.name === "account" ? (<IconContainerStyle onClick={this.handleClick}>
-                            <AccountIcon src={profilepic} />
-                        </IconContainerStyle>) : this.props.name
-                    }
-                    <Dropdown>
-                        <ItemMenu><Link to={{ pathname: '/profile', state: { highlights: true } }}>User Page</Link></ItemMenu>
-                        <ItemMenu><SignOutButton /></ItemMenu>
-                    </Dropdown>
-                </WrapperDropdown>
+                <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                    <DropdownToggle
+                        tag="span"
+                        onClick={this.toggle}
+                        data-toggle="dropdown"
+                        aria-expanded={this.state.dropdownOpen}
+                    >
+                        {   /* renders text or account icon */
+                            this.props.name === "account" ? (<IconContainerStyle onClick={this.handleClick}>
+                                <AccountIcon src={profilepic} />
+                            </IconContainerStyle>) : this.props.name
+                        }
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem><Link to={{ pathname: '/profile', state: { highlights: true } }}>User Page</Link></DropdownItem>
+                        <DropdownItem><SignOutButton /></DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
             </MenuBtnStyle>
         );
     }
-
 }
 
 export default MenuButton;
