0968ced0249717e1b7fef6d70d8d8ea3b27b717d
alizawren <alissaren98@gmail.com>
2018-12-02 23:30:54 -0800

    Pie chart working kind of

--- src/components/CompareMovies/ReviewCompare.js
+++ src/components/CompareMovies/ReviewCompare.js
@@ -1,61 +1,105 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import MovieService from '../../services/MovieService';
+import {Pie} from 'react-chartjs-2';
 
 class ReviewCompare extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-
+			movieTitles: [],
+			imdb: {},
+			metascore: {},
+			rotten_tomatoes: {}
 		}
 	}
 
 	componentDidMount() {
-		console.log(this.props.movies[0].imdb_id);
 		this.props.movies.forEach((movie) => {
+			var newMovieTitles = this.state.movieTitles;
+			newMovieTitles.push(movie.title);
+			this.setState({movieTitles: newMovieTitles});
 			var imdb_id = '';
-			console.log(movie.id);
 			MovieService.getSingleMovie(movie.id).then((movieData) => {
 				imdb_id = movieData.imdb_id;
-				console.log(imdb_id);
 				MovieService.getSingleMovieOMDb(imdb_id).then((movieData) => {
-					console.log(movieData);
+					//console.log(movieData);
+
+					const ratings = movieData.Ratings;
+					var rottenTomatoes = "N/A";
+					for (const source of ratings) {
+						if (source.Source === "Rotten Tomatoes") {
+							rottenTomatoes = source.Value;
+						}
+					}
+					var new_rotten_tomatoes = this.state.rotten_tomatoes;
+					new_rotten_tomatoes[movie.title] = rottenTomatoes;
+
+					var new_metascore = this.state.metascore;
+					new_metascore[movie.title] = movieData.Metascore;
+
+					var new_imdb = this.state.imdb;
+					new_imdb[movie.title] = movieData.imdbRating;
+		
+
+					this.setState({
+						rotten_tomatoes: new_rotten_tomatoes,
+						metascore: new_metascore,
+						imdb_rating: new_imdb
+					});
+
+					
 				})
 
-				/*
-				const ratings = movie.Ratings;
-				var rottenTomatoes = "N/A";
-				for (const source of ratings) {
-				  if (source.Source === "Rotten Tomatoes") {
-					rottenTomatoes = source.Value;
-				  }
-				}
-				var rated = movie.Rated;
-				if (rated === "N/A") {
-				  rated = "Not yet rated";
-				}
-				this.setState({
-				  director: movie.Director,
-				  actors: movie.Actors,
-				  runtime: movie.Runtime,
-				  rated: rated,
-				  rotten_tomatoes: rottenTomatoes,
-				  metascore: movie.Metascore,
-				  imdb_rating: movie.imdbRating
-				});*/
+
 			});
 		});
 
 	}
 
 	renderGraphs(chartType) {
+		var movieTitles = this.state.movieTitles;
+		var rottenTomatoesRatings = [];
+		for (var index in this.state.rotten_tomatoes) {
+			var ratingWithoutPercentage = parseInt(this.state.rotten_tomatoes[index].split('%')[0]);
+			rottenTomatoesRatings.push(ratingWithoutPercentage);	
+		}
+		console.log(movieTitles)
+		console.log(rottenTomatoesRatings)
+		const rottenTomatoesData = {
+			labels: movieTitles,
+			datasets: [{ 
+				data: rottenTomatoesRatings,
+				backgroundColor: [
+					'#44b7fb',
+					'#f55f81'
+				]
+			}]
+		};
+
+		const allRottenTomatoesPies = rottenTomatoesRatings.map(rating => {
+			var rating = parseInt(rating);
+			var otherRating = 100 - rating;
+			const data = {
+				labels: ['Liked it', 'Disliked it'],
+				datasets: [rating, otherRating],
+				backgroundColor: [
+					'#44b7fb',
+					'#f55f81'
+				]
+			}
+			return <Pie data={data} />
+		});
+
+		
+
 		if (chartType === "Rotten Tomatoes") {
 			return (
 				<div>
-					<p>Rotten Tomatoes</p>
+					<h1>Rotten Tomatoes</h1>
 					<div>
-
+						<Pie data={rottenTomatoesData}/>
 					</div>
 					<div>
 						<p>Avengers</p>
@@ -108,7 +152,9 @@ class ReviewCompare extends Component {
 
 
 	render() {
-
+		console.log(this.state.rotten_tomatoes)
+		console.log(this.state.imdb)
+		console.log(this.state.metascore)
 		return (
 			<div>
 				{this.renderGraphs(this.props.chartType)}
