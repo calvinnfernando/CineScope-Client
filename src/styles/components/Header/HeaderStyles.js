3bfe1dfca458f68f86db977e668cec640ed53c52
Bethany Bin <bethanybin@gmail.com>
2018-12-08 15:44:04 -0800

    more refactoring for components

---
+++ src/styles/components/Header/HeaderStyles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const LogoStyle = styled.div`
+margin: 10px auto;
+img {
+    height: 60px;
+    max-width: 100%;
+}
+`;
+
+const AccountIconStyle = styled.div`
+padding: 10px;
+`;
+
+export {LogoStyle, AccountIconStyle}
