b8cee6b90d1f39c6bd6cf14abb4cfa11e3604761
alizawren <alissaren98@gmail.com>
2018-12-09 00:55:52 -0800

    Last commit

--- src/styles/containers/AllMovies/StyledMovies.js
+++ src/styles/containers/AllMovies/StyledMovies.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 const StyledMovies = styled.div`
     padding: 10px;
-    margin: 10px;
+    margin: auto;
+    margin-top: 10px;
     border-radius: 15px;
     background-color: #e9ecef;
 `;
