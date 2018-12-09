d822663803904efaa388be73af4cc73322d3a09a
atmartorana <atmartorana@gmail.com>
2018-12-09 00:40:36 -0800

    Updates

---
+++ precache-manifest.be6de9e472a71e19f6fd93af6f981db1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d3f1783df641b28881184916eb2be72",
+    "url": "/projectcinescope/static/media/logo.5d3f1783.svg"
+  },
+  {
+    "revision": "43177cc5ef215f9dd65b",
+    "url": "/projectcinescope/static/css/main.3d7c9463.chunk.css"
+  },
+  {
+    "revision": "b48c9568a65d1a47224d338866c39a2d",
+    "url": "/projectcinescope/static/media/heart.b48c9568.png"
+  },
+  {
+    "revision": "80fc50c4d5c544055632",
+    "url": "/projectcinescope/static/js/1.80fc50c4.chunk.js"
+  },
+  {
+    "revision": "c1ad37104809f7ef36fc",
+    "url": "/projectcinescope/static/js/runtime~main.c1ad3710.js"
+  },
+  {
+    "revision": "ae4848674e133a1bbf6dc7698ac9a81a",
+    "url": "/projectcinescope/static/media/profile.ae484867.svg"
+  },
+  {
+    "revision": "43177cc5ef215f9dd65b",
+    "url": "/projectcinescope/static/js/main.43177cc5.chunk.js"
+  },
+  {
+    "revision": "2e188d70a0feeeac9fb60bf8f936c140",
+    "url": "/projectcinescope/static/media/avengers.2e188d70.jpg"
+  },
+  {
+    "revision": "5b2caefb44e84e9393b93da71f30c95d",
+    "url": "/projectcinescope/static/media/avengers2.5b2caefb.jpg"
+  },
+  {
+    "revision": "5a4881fe7a0e48e3e00e7f2ec480c2d6",
+    "url": "/projectcinescope/static/media/stranger.5a4881fe.jpg"
+  },
+  {
+    "revision": "57fce489fa7cfcf0258a2cd16969669b",
+    "url": "/projectcinescope/static/media/banner.57fce489.jpg"
+  },
+  {
+    "revision": "80fc50c4d5c544055632",
+    "url": "/projectcinescope/static/css/1.ad992f47.chunk.css"
+  },
+  {
+    "revision": "2afcd7e16f47ed57636ee1f1c6f55e92",
+    "url": "/projectcinescope/index.html"
+  }
+];
