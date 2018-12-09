f12e51af8f2536ae6f608f96374c3fbaee250f34
Bethany Bin <bethanybin@gmail.com>
2018-12-08 17:51:34 -0800

    fixed

---
+++ src/styles/containers/Login/TitleLogo.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+const TitleLogo = styled.div`
+    text-align: center;
+    font-family: 'Lobster', 'Verdana';
+    color: #FFFFFF;
+    font-size: 5em;
+    text-shadow: 2px 4px rgba(66, 31, 107, 0.2);
+`;
+
+export default TitleLogo
